995630024eb3713daae6078ea73ff384098cbe2f
Reinout <reinout.eyckerman@uantwerpen.be>
2019-06-19 16:15:58 +0200

    Refactor, reworked gameoverlay

--- The Game/The Game/UI/Overlay/GameOverlay.cs
+++ The Game/The Game/UI/Overlay/GameOverlay.cs
@@ -1,39 +1,18 @@
 ﻿using System;
 using Confusing_Hobo_Unleashed.Colors;
 using Confusing_Hobo_Unleashed.UI;
+using Confusing_Hobo_Unleashed.UI.UIElements;
 using Confusing_Hobo_Unleashed.User;
 
 namespace Confusing_Hobo_Unleashed
 {
     internal class GameOverlay:Drawable
     {
-        public GameOverlay()
+        private string playername;
+        private LoadBar hp;
+        public GameOverlay(string playername)
         {
             
-        }
-
-        public static void DrawOverlay(Buffer outputbuffer, Player player1 /*Player player2*/)
-        {
-            //This IF sets the maximum height of the UI to 5
-            if (_uiBotY - UiTopY > 7)
-            {
-                _uiBotY = UiTopY + 7;
-            }
-            Draw.FillRectangle(UiBoxColors, 0, Console.WindowWidth - 1, UiTopY - 2, Console.WindowHeight - 1, outputbuffer);
-            Draw.Box(UiLeftX, UiTopY, UiRightX, _uiBotY, UiBoxColors, outputbuffer);
-            DrawPlayerHp(outputbuffer, player1, "player 1", UiLeftX + 2, UiTopY + 2);
-        }
-
-        private static void DrawPlayerHp(Buffer outputbuffer, Player player, string playername, int xpos, int ypos)
-        {
-            outputbuffer.Draw(playername + " HP:", xpos, ypos, UiTextColors);
-            //Draw a 10-block wide hp-bar in red, this will be partially covered up by the green current hp.
-            outputbuffer.Draw("           ", xpos, ypos + 1, HpBarLostColor);
-            //Draw as many green blocks as currentHP/10 (100 HP = 10 Green blocks.)
-            for (var i = 0; i <= player.HpCurrent/10; i++)
-            {
-                outputbuffer.Draw(" ", xpos + i, ypos + 1, HpBarColor);
-            }
         }
 
         public void Draw()
